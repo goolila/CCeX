8185ce0e7b28a6496439d95ee2f9cb0bab0c8fb2
Ali Gholami <gholamia90@gmail.com>
2016-04-06 13:16:25 +0200

    set&check directories

--- ccex.py
+++ ccex.py
@@ -1,11 +1,32 @@
 import sys
 import os
+import collections
 
-# set input & output directory
-input_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), sys.argv[1])
-output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), sys.argv[2])
+# check if 2 arguments passed
+try:
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+except IndexError:
+    print "Usage: \tpython ccex.py <input_directory_name> <output_directory_name>"
+    sys.exit(1)
+
+# set input & output directories
+input_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), arg1)
+output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), arg2)
 
 # check if input & output directories are valid direcotories
 for d in [input_dir, output_dir]:
     if not os.path.isdir(d):
         print("Invalid directory:\n'%s' is not a valid direcotry, please insert a valid directory name in current path." %d)
+        sys.exit(1)
+
+def check_permission(input_dir, output_dir):
+    if not(os.access(output_dir, os.W_OK)):
+        os.chmod(output_dir, 0777)
+    if not(os.access(input_dir, os.R_OK)):
+        os.chmod(input_dir, 0644)
+
+if __name__ == "__main__":
+    # set and check input & output directories
+    check_permission(input_dir, output_dir)
+    print ("Input: %s\t is OK\nOutput: %s\tis OK" %(input_dir, output_dir))
