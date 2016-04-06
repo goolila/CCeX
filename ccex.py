f81d4f67ebef32fa2cd70bc634b29d52775abe94
Ali Gholami <gholamia90@gmail.com>
2016-04-06 21:34:12 +0200

    before adding lxml

--- ccex.py
+++ ccex.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import collections
 
 # check if 2 arguments passed
 try:
@@ -21,12 +20,30 @@ for d in [input_dir, output_dir]:
         sys.exit(1)
 
 def check_permission(input_dir, output_dir):
+    """
+    checks if input is readable and output is writeable
+    """
     if not(os.access(output_dir, os.W_OK)):
         os.chmod(output_dir, int(0777))
     if not(os.access(input_dir, os.R_OK)):
-        os.chmod(input_dir, int(0644))
+        os.chmod(input_dir, int(0744))
+
+files = os.listdir(input_dir)
+
+for f in files:
+    # f = 1-s2.0-S157082680400006X-full.xml
+    f_name, f_extension = os.path.splitext(f)
+    print f_name, f_extension
+    if (f_extension == ".xml" and f_name[-5:] == "-full"):
+        eid = f_name[0:-5] #1-s2.0-S157082680300009X
+        f_path = input_dir + f
+        print("Processing %s" %f_path)
+
+# Initialize counters
+number_of_papers = 0
+papers_with_no_crossrefs = 0
 
 if __name__ == "__main__":
     # set and check input & output directories
     check_permission(input_dir, output_dir)
-    print ("Input: %s\t is OK\nOutput: %s\tis OK" %(input_dir, output_dir))
+    # print ("Input: %s\t is OK\nOutput: %s\tis OK" %(input_dir, output_dir))
