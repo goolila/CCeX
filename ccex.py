3521ff18a15be1a822fe2ba75d152fdcb13cc16f
Ali Gholami <gholamia90@gmail.com>
2016-04-07 17:21:16 +0200

    expand ce:cross-refs

--- ccex.py
+++ ccex.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from lxml import etree
 
 # check if 2 arguments passed
 try:
@@ -28,6 +29,23 @@ def check_permission(input_dir, output_dir):
     if not(os.access(input_dir, os.R_OK)):
         os.chmod(input_dir, int(0744))
 
+namespaces = {'xocs' : 'http://www.elsevier.com/xml/xocs/dtd',
+    'ce' : 'http://www.elsevier.com/xml/common/dtd',
+    'xmlns' : "http://www.elsevier.com/xml/svapi/article/dtd",
+    'xmlns:xsi' : "http://www.w3.org/2001/XMLSchema-instance",
+    'xmlns:prism' : "http://prismstandard.org/namespaces/basic/2.0/",
+    'xmlns:dc' : "http://purl.org/dc/elements/1.1/",
+    'xmlns:xocs' : "http://www.elsevier.com/xml/xocs/dtd",
+    'xmlns:xlink' : "http://www.w3.org/1999/xlink",
+    'xmlns:tb' : "http://www.elsevier.com/xml/common/table/dtd",
+    'xmlns:sb' : "http://www.elsevier.com/xml/common/struct-bib/dtd",
+    'xmlns:sa' : "http://www.elsevier.com/xml/common/struct-aff/dtd",
+    'xmlns:mml' : "http://www.w3.org/1998/Math/MathML",
+    'xmlns:ja' : "http://www.elsevier.com/xml/ja/dtd",
+    'xmlns:ce' : "http://www.elsevier.com/xml/common/dtd",
+    'xmlns:cals' : "http://www.elsevier.com/xml/common/cals/dtd",
+}
+
 files = os.listdir(input_dir)
 
 for f in files:
@@ -38,6 +56,26 @@ for f in files:
         eid = f_name[0:-5] #1-s2.0-S157082680300009X
         f_path = input_dir + f
         print("Processing %s" %f_path)
+
+        tree = etree.parse(f_path)
+        print tree
+
+        """
+        Expand cross-refs as multiple adjacent <cross-ref refid=''> elements
+        """
+        xpath = "//ce:cross-refs"
+        cross_refs = tree.xpath(xpath, namespaces={'ce' : 'http://www.elsevier.com/xml/common/dtd'})
+        for cr in cross_refs:
+            cr_parent = cr.getparent()
+            ref_ids = cr.values()
+            for ri in ref_ids:
+                CE = "http://www.elsevier.com/xml/common/dtd"
+                NS_MAP = {'ce' : CE}
+                tag = etree.QName(CE, 'cross-refs')
+                exploded_cross_refs = etree.Element(tag, refid=ri nsmap=NS_MAP)
+                exploded_cross_refs.text = cr.text
+                cr.addprevious(exploded_cross_refs)
+            cr_parent.remove(cr)
 
 # Initialize counters
 number_of_papers = 0
